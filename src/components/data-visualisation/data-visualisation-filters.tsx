ba91db0f68e2fb5bc47b85cf46b24fdb752da7d8
Kevin Gallet <kevingallet@gmail.com>
2025-04-08 10:55:50 +0200

    feat: data viz - select epci by default (#20)

--- src/components/data-visualisation/data-visualisation-filters.tsx
+++ src/components/data-visualisation/data-visualisation-filters.tsx
@@ -1,23 +1,27 @@
 'use client'
 
 import Checkbox from '@codegouvfr/react-dsfr/Checkbox'
-import { parseAsArrayOf, parseAsString, useQueryState } from 'nuqs'
+import { parseAsArrayOf, parseAsString, useQueryStates } from 'nuqs'
 import { ComponentProps, FC, useEffect } from 'react'
 
 import { useDataVisualisation } from '~/hooks/use-data-visualisation'
 
 export const DataVisualisationFilters: FC = () => {
   const { data } = useDataVisualisation()
-  const [queryEpcis, setQueryEpcis] = useQueryState('epcis', parseAsArrayOf(parseAsString, ','))
+  const [queryStates, setQueryStates] = useQueryStates({
+    epcis: parseAsArrayOf(parseAsString, ','),
+    epci: parseAsString,
+  })
 
   const epcis = Object.keys(data?.linearChart ?? {}).map((epci) => data?.linearChart[epci].epci)
 
   useEffect(() => {
-    if (epcis.length > 0 && !queryEpcis) {
-      setQueryEpcis(epcis.map((epci) => epci.code))
+    if (!queryStates.epcis && queryStates.epci) {
+      setQueryStates({ epcis: [queryStates.epci] })
     }
-  }, [epcis, queryEpcis, setQueryEpcis])
+  }, [epcis, queryStates, setQueryStates])
 
+  const { epcis: queryEpcis } = queryStates
   const options: ComponentProps<typeof Checkbox>['options'] = epcis.map((epci) => ({
     label: epci.name,
     nativeInputProps: {
@@ -27,7 +31,7 @@ export const DataVisualisationFilters: FC = () => {
       onChange: (e) => {
         const currentEpcis = queryEpcis ?? []
         const newEpcis = e.target.checked ? [...currentEpcis, e.target.value] : currentEpcis.filter((epci) => epci !== e.target.value)
-        setQueryEpcis(newEpcis)
+        setQueryStates({ epcis: newEpcis })
       },
       value: epci.code,
     },

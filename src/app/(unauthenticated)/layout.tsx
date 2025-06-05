990fccde169bac4bb52af4a5a56eb8e29fd37338
Maxime Nguyen <maxime.nguyen@beta.gouv.fr>
2025-06-05 12:55:06 +0200

    fix layout

--- src/app/(unauthenticated)/layout.tsx
+++ src/app/(unauthenticated)/layout.tsx
@@ -1,12 +1,7 @@
 import { fr } from '@codegouvfr/react-dsfr'
 import { UnauthenticatedBreadcrumb } from '~/components/breadcrumbs/unauthenticated-breadcrumb'
 
-export default function UnauthenticatedLayout({
-  children,
-}: {
-  children: React.ReactNode
-  breadcrumb: React.ReactNode
-}) {
+export default function UnauthenticatedLayout({ children }: { children: React.ReactNode }) {
   return (
     <div style={{ backgroundColor: fr.colors.decisions.background.default.grey.default }}>
       <div className={fr.cx('fr-py-6v')}>

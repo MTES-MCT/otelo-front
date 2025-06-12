dd2bd9f39e47c8cf867d638582074e372c50b496
Kevin GALLET <kevingallet@gmail.com>
2025-06-12 13:59:45 +0200

    chore: remove prisma

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,6 +1,4 @@
-// import { PrismaAdapter } from '@auth/prisma-adapter'
 import NextAuth from 'next-auth'
-// import { AdapterAccount, AdapterUser } from 'next-auth/adapters'
 import { authOptions } from '~/lib/auth/auth.config'
 
 export const { auth, handlers, signIn, signOut } = NextAuth({

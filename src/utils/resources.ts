b59b0b667c202a15cc2793f4c4066447ebdc53d0
Maxime Nguyen <maxime.nguyen@gmail.com>
2025-08-12 10:18:41 +0200

    Add error handling and redirect flow in login form, update quiz URL (#68)
    Co-authored-by: Maxime Nguyen <maxime.nguyen@beta.gouv.fr>

--- src/utils/resources.ts
+++ src/utils/resources.ts
@@ -1,3 +1,3 @@
-export const QUIZ_URL = 'https://tally.so/r/3qe2z9'
+export const QUIZ_URL = 'https://tally.so/r/wzv0zq'
 export const WEBINAIRE_URL = 'https://tally.so/r/mZy2de'
 export const CONTACT_EMAIL = 'otelo@beta.gouv.fr'

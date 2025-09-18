6c062d5199eb25cdd3304821f7a6d2b3bec69270
Kevin GALLET <kevingallet@gmail.com>
2025-09-18 09:37:10 +0200

    feat: add route export powerpoint - wip

--- src/hooks/use-request-powerpoint.ts
+++ src/hooks/use-request-powerpoint.ts
@@ -3,15 +3,35 @@ import { TRequestPowerpoint } from '~/schemas/simulation'
 
 export const useRequestPowerpoint = () => {
   const postSimulation = async (params: TRequestPowerpoint) => {
-    const response = await fetch('/api/simulations/request-powerpoint', {
-      body: JSON.stringify(params),
-      method: 'POST',
-    })
+    console.log('params', params)
+    try {
+      const response = await fetch('/api/simulations/request-powerpoint', {
+        body: JSON.stringify(params),
+        method: 'POST',
+      })
 
-    if (!response.ok) {
-      throw new Error('Failed to request powerpoint')
+      if (!response.ok) {
+        throw new Error('Failed to request powerpoint')
+      }
+      const contentDisposition = response.headers.get('content-disposition')
+      const filename = contentDisposition
+        ? contentDisposition.split('filename=')[1].replace(/"/g, '')
+        : `scenario-${new Date().toISOString()}.xlsx`
+
+      const blob = await response.blob()
+
+      const url = window.URL.createObjectURL(blob)
+      const a = document.createElement('a')
+      a.href = url
+      a.download = filename
+      document.body.appendChild(a)
+      a.click()
+      window.URL.revokeObjectURL(url)
+      document.body.removeChild(a)
+    } catch (error) {
+      console.error('Failed to export simulation settings:', error)
+      throw error
     }
-    return response.json()
   }
 
   const { mutateAsync, isPending, isSuccess, isError } = useMutation({

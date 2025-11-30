3251c4841e364af842e1f32f9a17e7730200b5ed
Kevin GALLET <kevingallet@gmail.com>
2025-11-30 16:48:57 +0100

    fix: do not throw error on forgot-password endpoint

--- src/app/api/auth/forgot-password/route.ts
+++ src/app/api/auth/forgot-password/route.ts
@@ -2,17 +2,13 @@ import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
   const body = await request.json()
-  const res = await fetch(`${process.env.NEXT_OTELO_API_URL}/auth/forgot-password`, {
+  await fetch(`${process.env.NEXT_OTELO_API_URL}/auth/forgot-password`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(body),
   })
-
-  if (!res.ok) {
-    return NextResponse.json({ error: 'Failed to send forgot password email' }, { status: res.status })
-  }
 
   return NextResponse.json({ success: true })
 }
